efe70c5dfba865ae561b81a32993823bc4fce716
Tom Grushka <tom@dra11y.com>
2025-10-02 16:02:14 -0600

    remove legacy nesting_too_deep

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,6 @@ extern crate rustc_session;
 #[unsafe(no_mangle)]
 pub fn register_lints(sess: &rustc_session::Session, lint_store: &mut rustc_lint::LintStore) {
     eol_comments::register_lints(sess, lint_store);
-    // nesting_too_deep::register_lints(sess, lint_store);
     nesting_depth::register_lints(sess, lint_store);
     uninlined_format_args::register_lints(sess, lint_store);
 }
